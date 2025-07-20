c29499f3554c80575f4dc2c43d255377bf62dd30
Dagi2212 <167195227+Dagi2212@users.noreply.github.com>
2025-07-20 17:56:08 +0100

    Update Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 function Footer() {
   return (
-    <footer>
-      <p>© 2023 City Lovers</p>
+    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '15px', marginTop: '20px' }}>
+      <p>&copy; 2025 My Favorite Cities. All rights reserved.</p>
     </footer>
   );
 }
 
-export default Footer; 
+export default Footer;
