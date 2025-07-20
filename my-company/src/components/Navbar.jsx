8409849c4f22496f0f11dddf08175cd46f350f9e
Dagmawi Shewaye Nigusse <dagmawishewaye@gmail.com>
2025-07-20 10:40:02 +0100

    Update Navbar.jsx inline style to include backgroundColor, display, and justifyContent for checker compliance

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ background: '#333', padding: '10px' }}>
+    <nav style={{ backgroundColor: '#333', padding: '10px', display: 'flex', justifyContent: 'center' }}>
       <Link to="/" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Home</Link>
       <Link to="/about" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>About</Link>
       <Link to="/services" style={{ color: '#fff', margin: '0 10px', textDecoration: 'none' }}>Services</Link>
