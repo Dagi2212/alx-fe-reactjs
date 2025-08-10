536acac4807ad91a1d00dd06fb962cbf9d5706af
Dagi2212 <167195227+Dagi2212@users.noreply.github.com>
2025-08-10 14:31:32 +0300

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,3 +1,15 @@
+import UserProfile from "./components/UserProfile";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold text-blue-500">
